246207af74c692400ad4cccd2d2abdf308d8af6e
Jisu.seo <susjs90@gmail.com>
2023-02-26 18:34:54 +0900

    switch_개발중2

--- Arduino_Camera_Controller.ino
+++ Arduino_Camera_Controller.ino
@@ -1,8 +1,22 @@
 
-#include <ShiftRegister74HC595.h>
 
-// parameters: <number of shift registers> (data pin, clock pin, latch pin)
-//ShiftRegister74HC595<1> sr(4, 2, 3);
+#define SWITCH_PUSH     (0)
+#define SWITCH_NONE     (1)
+
+#define SWITCH_REQ_ON   (1)
+#define SWITCH_REQ_OFF  (0)
+
+#define MOTOR_INIT      (0)
+#define MOTOR_MEMORY_1  (1)
+#define MOTOR_MEMORY_2  (2)
+#define MOTOR_MEMORY_3  (3)
+#define MOTOR_MEMORY_4  (4)
+#define MOTOR_MEMORY_5  (5)
+#define MOTOR_SAVE_1    (6)
+#define MOTOR_SAVE_2    (7)
+#define MOTOR_SAVE_3    (8)
+#define MOTOR_SAVE_4    (9)
+#define MOTOR_SAVE_5    (10)
 
 const uint8_t OutLatchPin = 3;
 const uint8_t OutDataPin = 4;
@@ -12,9 +26,11 @@ const uint8_t InClockPin = 12;
 const uint8_t InClockEnPin = 9;
 const uint8_t InDataPin = 11;
 
-int8_t Led_Status;
+uint8_t Led_Right,Led_M1,Led_M2,Led_M3,Led_M4,Led_M5,Output_Trigger;
 uint16_t X_Position,Y_Position = 999;
-uint8_t Switch_Status1,Switch_Status2;
+uint8_t Switch_Y_D,Switch_Y_U,Switch_X_D,Switch_X_U,Switch_Start,Switch_Finish;
+uint8_t Switch_M1,Switch_M2,Switch_M3,Switch_M4,Switch_M5,Switch_Right;
+uint8_t Motor_Memory_Status;
 const uint8_t Segment_Num[] = 
 {
     0b00111111, //0
@@ -43,8 +59,6 @@ void setup()
     pinMode(InDataPin, INPUT);
 
     digitalWrite(InClockEnPin, LOW); 
-    digitalWrite(InClockPin, HIGH); 
-    digitalWrite(InShiftPin, HIGH);
 }
 
 void loop() 
@@ -53,9 +67,56 @@ void loop()
     delay(5);
     Data_Output();
     delay(5);
-    Serial.print(Switch_Status1);
+    Motor_Control();
+    Serial.print(Switch_Y_D);
     Serial.print(", ");
-    Serial.println(Switch_Status2);
+    Serial.println(Switch_Y_U);
+}
+
+static void Motor_Control(void)
+{
+    Led_M1 = 1;
+    Led_M2 = 1;
+    Led_M3 = 1;
+    Led_M4 = 1;
+    Led_M5 = 1;
+}
+
+static void Switch_Control(void)
+{
+    static uint8_t stM1Old,stM2Old,stM3Old,stM4Old,stM5Old,stRightOld;
+
+    if ((stRightOld == SWITCH_NONE) && (Switch_Right == SWITCH_PUSH))
+    {
+        Led_Right = (~Led_Right) & 0x01;
+    }
+    stRightOld = Switch_Right;
+    if ((stM1Old == SWITCH_NONE) && (Switch_M1 == SWITCH_PUSH))
+    {
+        Motor_Memory_Status = MOTOR_MEMORY_1;
+    }
+    stM1Old = Switch_M1;
+    if ((stM2Old == SWITCH_NONE) && (Switch_M2 == SWITCH_PUSH))
+    {
+        Motor_Memory_Status = MOTOR_MEMORY_2;
+    }
+    stM2Old = Switch_M2;
+    if ((stM3Old == SWITCH_NONE) && (Switch_M3 == SWITCH_PUSH))
+    {
+        Motor_Memory_Status = MOTOR_MEMORY_3;
+    }
+    stM3Old = Switch_M3;
+    if ((stM4Old == SWITCH_NONE) && (Switch_M4 == SWITCH_PUSH))
+    {
+        Motor_Memory_Status = MOTOR_MEMORY_4;
+    }
+    stM4Old = Switch_M4;
+    if ((stM5Old == SWITCH_NONE) && (Switch_M5 == SWITCH_PUSH))
+    {
+        Motor_Memory_Status = MOTOR_MEMORY_5;
+    }
+    stM5Old = Switch_M5;    
+
 }
 
 static void Data_Output(void)
@@ -92,7 +153,8 @@ static void Data_Output(void)
             SegOut = 0b00110110; 
         break;
     }
-    LED_Out = Led_Status;
+    LED_Out = (Led_Right & 0x01) | ((Led_M1 & 0x01) << 1) | ((Led_M2 & 0x01) << 2) | ((Led_M3 & 0x01) << 3) | 
+              ((Led_M4 & 0x01) << 4) | ((Led_M5 & 0x01) << 5) | ((Output_Trigger & 0x01) << 6);
 
     digitalWrite(OutLatchPin,LOW);
     shiftOut(OutDataPin, OutClockPin, MSBFIRST, X_Out);
@@ -110,23 +172,43 @@ static void Data_Output(void)
 
 static void Data_Input(void)
 {
-    uint8_t bitVal,bytesVal;
-    static uint8_t index;
+    uint8_t Switch_Status1,Switch_Status2;
+    Data_Read_Begin();
+    Switch_Status1 = Data_Read();
+    Switch_Status2 = Data_Read();
 
-    digitalWrite(InClockPin, HIGH); 
+    Switch_M5 = (Switch_Status1 >> 0) & 0x01;
+    Switch_M4 = (Switch_Status1 >> 1) & 0x01;
+    Switch_M3 = (Switch_Status1 >> 2) & 0x01;
+    Switch_M2 = (Switch_Status1 >> 3) & 0x01;
+    Switch_M1 = (Switch_Status1 >> 4) & 0x01;
+    Switch_Right = (Switch_Status1 >> 5) & 0x01;
+
+    Switch_Y_D = (Switch_Status2 >> 0) & 0x01;
+    Switch_Y_U = (Switch_Status2 >> 1) & 0x01;
+    Switch_X_D = (Switch_Status2 >> 2) & 0x01;
+    Switch_X_U = (Switch_Status2 >> 3) & 0x01;
+    Switch_Start = (Switch_Status2 >> 4) & 0x01;
+    Switch_Finish = (Switch_Status2 >> 5) & 0x01;
+}
+static uint8_t Data_Read_Begin(void)
+{
     digitalWrite(InShiftPin, LOW); 
     delayMicroseconds(5);
     digitalWrite(InShiftPin, HIGH); 
-  
+}
 
-    for(int i = 0; i < 8; i++)
+static uint8_t Data_Read(void)
+{
+    uint8_t result = 0;
+    for (uint8_t i = 0; i < 8; i++)
     {
-        Switch_Status1 = shiftIn(InDataPin, InClockPin, MSBFIRST);
-    }
+        uint8_t value = digitalRead(InDataPin);
+        result |= (value << ((8 - 1) - i));
 
-    for(int i = 0; i < 8; i++)
-    {
-        Switch_Status2 = shiftIn(InDataPin, InClockPin, MSBFIRST);
+        digitalWrite(InClockPin,HIGH);
+        delayMicroseconds(5);
+        digitalWrite(InClockPin,LOW);
     }
-
+    return result;
 }
